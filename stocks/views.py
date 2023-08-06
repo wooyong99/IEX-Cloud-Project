a0b765b1f6110598ddc6e171385b90547c9a7f7c
wooyong99 <wulovesk@naver.com>
2023-08-06 15:34:21 +0900

    필요 데이터 추출 및 노출

--- stocks/views.py
+++ stocks/views.py
@@ -17,11 +17,10 @@ def search(request):
         word = request.GET.get('word')[:4]
 
         stock_api = requests.get("https://api.iex.cloud/v1/data/core/quote/{0}?token=pk_8d7f88195da14dc396d5f8a2521a0624".format(word))
-
-        stock = json.loads(stock_api.content)
+        stock = json.loads(stock_api.content)[0]
     except Exception as e:
         stock = ""
 
-    content = {'stock' : stock}
+    content = {'stock': stock}
 
     return render(request, "stocks/search.html", content)
