8447e7d3ee0b8d7b010f6babca35782562a5ee24
wooyong99 <wulovesk@naver.com>
2023-08-06 14:33:49 +0900

    주식 api 적용 및 회사 검색 기능 구현

--- stocks/views.py
+++ stocks/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import requests
+import json
 
-# Create your views here.
+def home(request):
+
+    stock_api = requests.get("https://api.iex.cloud/v1/data/core/quote/goog?token=pk_8d7f88195da14dc396d5f8a2521a0624")
+    stock = json.loads(stock_api.content)
+
+    content = {'stock': stock}
+
+    return render(request, 'stocks/home.html', content)
+
+def search(request):
+    word = request.GET.get('word')
+    stock_api = requests.get("https://api.iex.cloud/v1/data/core/quote/{0}?token=pk_8d7f88195da14dc396d5f8a2521a0624".format(word[:4]))
+
+    stock = json.loads(stock_api.content)
+
+    content = {'stock' : stock}
+    return render(request, "stocks/search.html", content)
